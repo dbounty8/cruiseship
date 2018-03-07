b667808c7634ee9e2bba7f427684f81e43be6f55
David Sheffield <davesheff@live.co.uk>
2018-03-07 19:46:10 +0000

    startingPort property added to the Ship constructor

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -4,4 +4,9 @@ describe('Ship', () => {
   test('it can be instantiated', () => {
     expect(new Ship).toBeInstanceOf(Object);
   });
+
+  test('Sets the startingPort property', () => {
+    const ship = new Ship('Dover');
+    expect(ship.startingPort).toEqual('Dover');
+  });
 });
